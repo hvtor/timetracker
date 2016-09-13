b212239fb0db33bb8f1815adc736c0038a96d350
Hemant Torsekar <hvtorsekar@gmail.com>
2016-09-13 15:05:50 -0400

    Added labelsContainerView and setup for lcv.

--- Pomodoro/ViewController.swift
+++ Pomodoro/ViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let labelsContainerView: UIView = {
+        let lcv = UIView()
+        lcv.backgroundColor = UIColor.white
+        lcv.translatesAutoresizingMaskIntoConstraints = false
+        
+        return lcv
+    }()
+    
+    let buttonsContainerView: UIView = {
+        let bcv = UIView()
+        bcv.backgroundColor = UIColor.blue
+        bcv.translatesAutoresizingMaskIntoConstraints = false
+        
+        return bcv
+    }()
+    
+    
+    
+    let timerLabel : UILabel = {
+        let tl = UILabel()
+        let font = UIFont(name: "HelveticaNeue-Thin", size: 64.0)
+        tl.font = font
+        return tl
+    }()
+    
+    let taskLabel : UILabel = {
+        let tl = UILabel()
+        
+        return tl
+        
+    }()
     
     let logTimeButton: ActionButton = {
         let ltb = ActionButton()
@@ -17,13 +48,32 @@ class ViewController: UIViewController {
         return ltb
     }()
     
+    
+    
+    let playPauseButton: PlayPauseButton = {
+        let ppb = PlayPauseButton()
+        
+        return ppb
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
      
         view.backgroundColor = UIColor(hexString: "#003554", alpha: 1.0)
         
-        
+        view.addSubview(labelsContainerView)
+        view.addSubview(buttonsContainerView)
+        setupLabelsContainerView()
     }
+    
+    func setupLabelsContainerView() {
+        let topAnchorHeight = UIApplication.shared.statusBarFrame.height + 20
+        labelsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        labelsContainerView.topAnchor.constraint(equalTo: view.topAnchor, constant:topAnchorHeight).isActive = true
+        labelsContainerView.widthAnchor.constraint(equalTo:view.widthAnchor, constant:-20).isActive = true
+        labelsContainerView.heightAnchor.constraint(equalTo:view.widthAnchor, multiplier: 0.75).isActive = true
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
