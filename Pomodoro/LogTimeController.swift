ac3566204a1239669d0cb48573cc2aebfa13a700
Hemant Torsekar <hvtorsekar@gmail.com>
2016-09-14 06:10:44 -0400

    Added downArrow to LogTimeVC.

--- Pomodoro/LogTimeController.swift
+++ Pomodoro/LogTimeController.swift
@@ -10,22 +10,46 @@ import UIKit
 
 class LogTimeController: UIViewController {
     
-    let logTimeView: UIVisualEffectView = {
-        let v = UIVisualEffectView()
+    let logTimeView: UILabel = {
+        let v = UILabel()
             v.layer.cornerRadius = 10
-            
+            v.backgroundColor = UIColor(hexString: "#0582ca", alpha: 1.0)
+            v.alpha = 0.5
             v.translatesAutoresizingMaskIntoConstraints = false
         return v
+    }()
+    
+    let downArrow: UIButton = {
+        let da = UIButton()
+        da.setImage(UIImage(named:"downArrow.png"), for: .normal)
+        da.titleShadowColor(for: .normal)
+        da.translatesAutoresizingMaskIntoConstraints = false
+        return da
     }()
     
     let inputsContainerView: UIScrollView = {
         let iv = UIScrollView()
         
-            iv.layer.backgroundColor = UIColor(hexString: "#006494", alpha: 1.0).cgColor
-            iv.alpha = 0.95
+            iv.backgroundColor = UIColor(hexString: "#006494", alpha: 1.0)
+            iv.alpha = 0.97
             iv.layer.cornerRadius = 5
             iv.translatesAutoresizingMaskIntoConstraints = false
         return iv
+    }()
+    
+    let timerValue: UITextField = {
+        let tv = UITextField()
+        
+        tv.backgroundColor = UIColor(hexString: "#FAFAFA", alpha: 1.0)
+        tv.placeholder = ""
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        return tv
+    }()
+    
+    let projectName: UITextField = {
+        let pn = UITextField()
+        pn.translatesAutoresizingMaskIntoConstraints = false
+        return pn
     }()
     
     
@@ -39,8 +63,19 @@ class LogTimeController: UIViewController {
         logTimeView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         logTimeView.topAnchor.constraint(equalTo: view.topAnchor, constant:160).isActive = true
         logTimeView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -40).isActive = true
-        logTimeView.heightAnchor.constraint(equalTo: view.heightAnchor, constant: -180).isActive = true
-        
+        logTimeView.heightAnchor.constraint(equalTo: inputsContainerView.heightAnchor).isActive = true
+    }
+    
+    func setupTextFields() {
+    
+    
+    }
+    
+    func setupDownArrow() {
+        downArrow.topAnchor.constraint(equalTo: inputsContainerView.topAnchor, constant:10).isActive = true
+        downArrow.centerXAnchor.constraint(equalTo: inputsContainerView.centerXAnchor).isActive = true
+        downArrow.heightAnchor.constraint(equalTo: downArrow.heightAnchor, constant:40).isActive = true
+        downArrow.widthAnchor.constraint(equalTo: downArrow.widthAnchor, constant: 160).isActive = true
     }
 
     override func viewDidLoad() {
@@ -48,11 +83,14 @@ class LogTimeController: UIViewController {
 
         view.addSubview(inputsContainerView)
         inputsContainerView.addSubview(logTimeView)
+        inputsContainerView.addSubview(downArrow)
         let vev = UIVisualEffectView()
         logTimeView.addSubview(vev)
         
         setupInputsView()
         setupLogTimeView()
+        setupDownArrow()
+        
         // Do any additional setup after loading the view.
     }
 
